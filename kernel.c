a949d2e45e4ac2f373954c18da7ad088e13a89fc
xuepeng <2082994803@qq.com>
2026-03-20 09:46:33 +0800

    feat:输出组长薛鹏的信息

--- kernel.c
+++ kernel.c
@@ -4,7 +4,8 @@ extern void uart_puts(char *s);
 void start_kernel(void)
 {
 	uart_init();
-	uart_puts("Hello, World!\n");
+	uart_puts("Hello, Kernel!\n");
+	uart_puts("My name is 薛鹏\n");
 
 	while (1) {}; // stop here!
 }
