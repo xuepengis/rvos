b769638268b73921d5b05e38afc8d073df119fae
xuepeng <yangyixuan0829@qq.com@example.com>
2026-04-10 19:27:23 +0800

    feat:Fix interrupt enable timing

--- sched.c
+++ sched.c
@@ -107,7 +107,7 @@ int task_create(void (*start_routin)(void), int priority)
 {
 	if (_top < MAX_TASKS) {
 		ctx_tasks[_top].ctx.sp = (reg_t) &task_stack[_top][STACK_SIZE];
-		// ctx_tasks[_top].ctx.ra = (reg_t) start_routin;
+		ctx_tasks[_top].ctx.ra = (reg_t) start_routin;
         // 👇 新增 pc 初始化，指向任务的起始地址
 		ctx_tasks[_top].ctx.pc = (reg_t) start_routin; 
 		ctx_tasks[_top].priority = priority;
--- timer.c
+++ timer.c
@@ -27,8 +27,8 @@ void timer_init()
 	/* enable machine-mode timer interrupts. */
 	w_mie(r_mie() | MIE_MTIE);
 
-	/* enable machine-mode global interrupts. */
-	w_mstatus(r_mstatus() | MSTATUS_MIE);
+	/* 移除这一行：不要过早开启全局中断 */
+	// w_mstatus(r_mstatus() | MSTATUS_MIE); 
 }
 
 void timer_handler() 
